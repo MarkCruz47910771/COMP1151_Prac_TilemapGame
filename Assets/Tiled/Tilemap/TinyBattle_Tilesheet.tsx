649685606368cb16b19cf6345a41464d3a7a2534
MarkCruz47910771 <mark.cruz@students.mq.edu.au>
2025-05-30 11:36:09 +1000

    Main game added

---
+++ Assets/Tiled/Tilemap/TinyBattle_Tilesheet.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="TinyBattle_Tilesheet" tilewidth="16" tileheight="16" spacing="1" tilecount="198" columns="18">
+ <image source="tilemap.png" width="305" height="186"/>
+ <tile id="27">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="63">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+   <object id="3" x="0" y="0" width="16" height="16"/>
+   <object id="4" x="0" y="0" width="16" height="16"/>
+   <object id="5" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
